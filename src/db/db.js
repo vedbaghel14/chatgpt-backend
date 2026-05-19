4b9e65aa89d2e1ea58b6b75c635a478998f22872
vedbaghel14 <baghelved341@gmail.com>
2026-05-20 01:59:15 +0530

    changed some privacy

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ dns.setServers(['1.1.1.1','8.8.8.8'])
 
 async function connectDb(){
     try{
-        await mongoose.connect("mongodb+srv://ved:c5aFjygoHf33nuVa@cluster0.i8pnc0v.mongodb.net/chatgpt")
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log("connected to database")
     }
     catch(err){
